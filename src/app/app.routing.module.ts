a0479333d7e09e42c7b072fa3a3a614258a147c1
Dereck Vinicius Silva <viniciusdereck39@gmail.com>
2022-11-05 20:39:26 -0300

    routes/css adjustment

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,9 +1,12 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { SideBarComponent } from "./side-bar/side-bar.component";
+import { ClientsComponent } from './clients/clients.component';
 
 const routes: Routes = [
-    {path: 'home', component: SideBarComponent}
+    {path: 'home', component: HomeComponent},
+    {path:'clientes', component: ClientsComponent}
 ]
 
 @NgModule({
--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { faHome,faHouse } from '@fortawesome/free-solid-svg-icons';
+import { Component } from '@angular/core';
+import { faHouse,faUsers,faShop,faCoins,faChartSimple,faCalendarDay,faUtensils,faUserTie,faRocket } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
-export class SideBarComponent implements OnInit {
+export class SideBarComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
-  faHome = faHome
   faHouse = faHouse
+  faUsers = faUsers
+  faShop = faShop
+  faCoins = faCoins
+  faChartSimple = faChartSimple
+  faCalendarDay = faCalendarDay
+  faUtensils = faUtensils
+  faUserTie = faUserTie
+  faRocket = faRocket
 
 }
